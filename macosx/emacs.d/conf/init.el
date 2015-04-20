c996437570e2096436d05f9b9f5e23fb760ced3a
nobjas <nobjas@gmail.com>
2015-04-20 12:34:23 +0900

    細かく設定調整した

--- macosx/emacs.d/conf/init.el
+++ macosx/emacs.d/conf/init.el
@@ -36,8 +36,8 @@
   "Go to the matching paren if on a paren; otherwise insert %."
   (interactive "p")
   (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-	(t (self-insert-command (or arg 1)))))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
 (define-key global-map (kbd "%") 'match-paren)
 
 ;; 色つける
