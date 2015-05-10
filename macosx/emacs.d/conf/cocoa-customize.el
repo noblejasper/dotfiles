511469dd5423cac10abe301c0245669c439e2b05
nobjas <nobjas@gmail.com>
2015-05-10 11:14:35 +0900

    bashrcのチェック入れたら他の設定が死んだのでナシにした

--- macosx/emacs.d/conf/cocoa-customize.el
+++ macosx/emacs.d/conf/cocoa-customize.el
@@ -3,7 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ansi-color-names-vector
+   ["#232728" "#ff017e" "#82c600" "#fff269" "#6aa4f7" "#ac59ff" "#6aa4f7" "#d2d2d2"])
+ '(ansi-term-color-vector
+   [unspecified "#232728" "#ff017e" "#82c600" "#fff269" "#6aa4f7" "#ac59ff" "#6aa4f7" "#d2d2d2"])
+ '(coffee-args-compile (quote ("-c" "--bare")))
+ '(coffee-tab-width 4)
+ '(custom-safe-themes
+   (quote
+    ("5e1d45c4639130d82077749c5dacaca796666657a876c2c3f30835b093fd5942" default)))
+ '(yas-trigger-key "TAB"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
